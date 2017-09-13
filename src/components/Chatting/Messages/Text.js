5a9ff072532dfd4382e33ba3e34299af4bc1d78a
dgeibi <png.inside@gmail.com>
2017-09-14 01:01:30 +0800

    change date format

--- src/components/Chatting/Messages/Text.js
+++ src/components/Chatting/Messages/Text.js
@@ -6,7 +6,7 @@ const Text = ({ username, text, myName, self, date }) => (
   <div styleName="text">
     <header styleName="text__header">{self ? myName : username}</header>
     <main styleName="text__main">{text}</main>
-    <footer styleName="text__footer">{dateFormat(date, 'HH:MM')}</footer>
+    <footer styleName="text__footer">{dateFormat(date, 'yyyy-mm-dd HH:MM')}</footer>
   </div>
 )
 
