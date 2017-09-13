ef644088f913dcbd5357b2e043f65998c52c897c
dgeibi <png.inside@gmail.com>
2017-09-14 00:54:36 +0800

    use builder api

--- build/build-win-portable.js
+++ build/build-win-portable.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const { writeFileSync, mkdirpSync } = require('fs-extra')
-const { resolve, join } = require('path')
-const { shellSync } = require('p2p-chat-utils/shell')
 const commonConfig = require('../package').build
+const builder = require('electron-builder')
+
+const Platform = builder.Platform
 
 const createConfig = arch =>
   Object.assign({}, commonConfig, {
@@ -20,16 +20,11 @@ const createConfig = arch =>
     },
   })
 
-const createFile = (arch) => {
-  const distDir = resolve(__dirname, '../dist')
-  mkdirpSync(distDir)
-
-  const config = createConfig(arch)
-  const filename = join(distDir, `/win-${arch}.json`)
-  writeFileSync(filename, JSON.stringify(config))
-  return filename
-}
-
 const arch = process.argv[2] || 'ia32'
-const buildBin = resolve(__dirname, '../node_modules/.bin/build')
-shellSync(`${buildBin} -w -c ${createFile(arch)}`)
+createConfig(arch)
+
+// Promise is returned
+builder.build({
+  targets: Platform.WINDOWS.createTarget(),
+  config: createConfig(arch),
+})
