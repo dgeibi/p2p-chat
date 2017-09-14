a79cb449a2b4fc0a3e02cf53c97f9dd5a685f558
dgeibi <png.inside@gmail.com>
2017-09-14 17:21:24 +0800

    message: show tag for other users

--- src/components/Chatting/Messages/Text.js
+++ src/components/Chatting/Messages/Text.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import dateFormat from 'dateformat'
+
+import { formatTag } from '../../../utils/format'
+
 import './Text.scss'
 
-const Text = ({ username, text, myName, self, date }) => (
+const Text = ({ username, text, myName, self, date, tag }) => (
   <div styleName="text">
-    <header styleName="text__header">{self ? myName : username}</header>
+    <header styleName="text__header">{self ? myName : username + formatTag(tag)}</header>
     <main styleName="text__main">{text}</main>
     <footer styleName="text__footer">{dateFormat(date, 'yyyy-mm-dd HH:MM')}</footer>
   </div>
