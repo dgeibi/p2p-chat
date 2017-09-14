550d36d24b5bf43b3431ab6ccd7c234b57b4e162
dgeibi <png.inside@gmail.com>
2017-09-14 22:48:55 +0800

    try fix build mistake

--- build/build-win-portable.js
+++ build/build-win-portable.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const { renameSync, existsSync } = require('fs')
+const { resolve, join } = require('path')
 const commonConfig = require('../package').build
 const builder = require('electron-builder')
 
@@ -19,6 +21,20 @@ const createConfig = arch =>
       artifactName: `\${productName}-portable-win-\${version}-${arch}.\${ext}`,
     },
   })
+
+const ensureOld = (BASE_DIR, name) => {
+  const oldPath = join(BASE_DIR, name)
+  if (existsSync(oldPath)) {
+    renameSync(oldPath, join(BASE_DIR, `${name}.old`))
+  }
+}
+
+const DIST_DIR = resolve(__dirname, '../dist')
+if (existsSync(DIST_DIR)) {
+  ensureOld(DIST_DIR, 'linux-unpacked')
+  ensureOld(DIST_DIR, 'win-ia32-unpacked')
+  ensureOld(DIST_DIR, 'win-ia32')
+}
 
 const arch = process.argv[2] || 'ia32'
 
