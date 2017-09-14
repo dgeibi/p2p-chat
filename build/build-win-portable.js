cb4a732e5274177b34717d17e920c1e8d3d7b24d
dgeibi <png.inside@gmail.com>
2017-09-14 13:43:03 +0800

    rename to fix build mistake

--- build/build-win-portable.js
+++ build/build-win-portable.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const { renameSync, existsSync } = require('fs')
+const { resolve, join } = require('path')
 const commonConfig = require('../package').build
 const builder = require('electron-builder')
 
@@ -20,10 +22,21 @@ const createConfig = arch =>
     },
   })
 
-const arch = process.argv[2] || 'ia32'
-createConfig(arch)
+const ensureOld = (BASE_DIR, name) => {
+  const oldPath = join(BASE_DIR, name)
+  if (existsSync(oldPath)) {
+    renameSync(oldPath, join(BASE_DIR, `${name}.old`))
+  }
+}
 
-// Promise is returned
+const DIST_DIR = resolve(__dirname, '../dist')
+if (existsSync(DIST_DIR)) {
+  ensureOld(DIST_DIR, 'win-ia32-unpacked')
+  ensureOld(DIST_DIR, 'win-ia32')
+}
+
+const arch = process.argv[2] || 'ia32'
+
 builder.build({
   targets: Platform.WINDOWS.createTarget(),
   config: createConfig(arch),
